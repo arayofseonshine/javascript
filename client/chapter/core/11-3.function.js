540899f3a044e054413d7c3660aef49cb6982a6a
arayofseonshine <arayofseonshine@naver.com>
2024-06-07 13:43:46 +0900

    2024년 6월 7일

--- client/chapter/core/11-3.function.js
+++ client/chapter/core/11-3.function.js
@@ -10,9 +10,9 @@ const calculateTotal = function(moneyA, moneyB, moneyC, moneyD) {
   let resultY = calculateTotal(21500, 3200, 9800, 4700);
   let resultZ = calculateTotal(9000, -2500, 5000, 11900);
   
-  console.log(resultX);
-  console.log(resultY);
-  console.log(resultZ);
+//   console.log(resultX);
+//   console.log(resultY);
+//   console.log(resultZ);
   
   // functionsum(... args){
 
@@ -112,11 +112,12 @@ console.log(result);
 // 일반함수 
 // - constructor 내장
 // - this : 나를 호출한 대상 this
-// - 객체의 매서드로 사용이 많이 됨 => this를 찾기 위해
+// ★ - 객체의 매서드로 사용이 많이 됨 => this를 찾기 위해
 
 // 화살표 함수 
 // - constructor 비내장
 // - this : 바인딩 하지 않음 -> 상위 컨텍스트에서 this 찾음. 
+// ★  - 매서드 안의 함수를 작성해야 할 때 (내 상위 this를 가져오기 때문에)
 
 // 예시 1) 
 // const user = {
@@ -131,6 +132,7 @@ console.log(result);
 // 		console.log(this);
 // 	}
 // } 
+
 
 // 예시 2)
 // const user = {
@@ -166,30 +168,106 @@ console.log(result);
 
 
 // 예시3 변형
-const user = {
-	name: '박새롬',
-	total: 0,
-	grades: [30, 60, 80],
-	totalGrades(){
+// const user = {
+// 	name: '박새롬',
+// 	total: 0,
+// 	grades: [30, 60, 80],
+// 	totalGrades(){
 		
-		const sayHi = () => {
-			console.log(this);
-		}
+// 		const sayHi = () => {
+// 			console.log(this);
+// 		}
 
-		sayHi()
-	}
+// 		sayHi()
+// 	}
+// }
+
+
+// const user = {
+// 	name: '정은선',
+// 	total: 0,
+// 	grades:[30,60,80],
+// 	totalGrades(){
+
+// 		this.grades.forEach(funciton(item){
+// 			console.log(this);
+// 		})
+// 	}
+// }
+// 화살표함수 변형
+// const user = {
+// 	name: '정은선',
+// 	total: 0,
+// 	grades:[30,60,80],
+// 	totalGrades(){
+
+// 		this.grades.forEach((item)=>{
+// 			console.log(this);
+// 		})
+
+// 	}
+// }
+
+// user.totalGrades()
+
+// function forEach(func){
+
+// 	func()
+
+// }
+
+// forEach(function(){})
+
+
+
+
+  
+/* 다음 함수를 작성해봅니다. -------------------------------------------------- */
+
+// pow(numeric: number, powerCount: number): number;
+// let pow = (numeric,powerCount)=>{
+
+// 	let result = 1;
+// 	for(let i = 0; i < powerCount; i++){
+// 		result *= numeric
+// 	}
+
+// 	return result;
+	
+// };
+
+// 표현식 다른 버전 
+// const pow = (numeric,powerCount) => {
+
+// 	return Array(powerCount).fill(null).reduce((acc,cur)=>{
+// 		return acc *= numeric
+// 	},1)
+
+// }
+
+// const pow = (numeric,powerCount) => Array(powerCount).fill(null).reduce(acc => acc *= numeric,1)
+
+
+ // repeat(text: string, repeatCount: number): string;
+ // 반복문 
+//  let repeat = (text,repeatCount)=>{
+// 	let result = '';
+// 	for(let i = 0; i < repeatCount; i++){
+// 		result += text;
+// 	}
+// 	return result
+//   }
+
+//   repeat('안녕~',3) ; // 안녕안녕안녕
+
+
+// 다른 버전
+const repeat = (text,repeatCount)=>{
+	return Array(repeatCount).fill(null).reduce((acc)=>{
+		return acc + text
+	},'')
 }
+repeat('사랑해!',3);
 
-
-
-
-
-
-  
-  /* 다음 함수를 작성해봅니다. -------------------------------------------------- */
-  
-  // pow(numeric: number, powerCount: number): number;
-  let pow; 
-  
-  // repeat(text: string, repeatCount: number): string;
-  let repeat; 
+// 한줄로 바꾸면
+// const repeat = (text, repeatCount) => Array(repeatCount).fill(null).reduce((acc)=> acc + text, '')
