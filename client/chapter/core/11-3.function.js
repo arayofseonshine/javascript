6d810e4ada423b280e6a8928429ea5f92a94caa7
arayofseonshine <arayofseonshine@naver.com>
2024-06-07 11:39:13 +0900

    2024년 6월 7일

--- client/chapter/core/11-3.function.js
+++ client/chapter/core/11-3.function.js
@@ -78,15 +78,113 @@ let calcAllMoney = (... rest) => {
 	// 	return acc + cur
 	// },0);
 	// < reduce => arrow function으로 >
-	return rest.reduce((acc,cur) => acc + cur,0);
+	// return rest.reduce((acc,cur) => acc + cur,0);
 
 };
 const result = calcAllMoney(1000, 5000, 4500, 13000);
 console.log(result);
 
 
+
   // 화살표 함수와 this
-  
+//   function create(nickName,number){
+
+// 	return {
+// 		name:nickName,
+// 		age:number
+// 	}
+
+//   }
+//   create('tiger', 40);
+
+// 자바스크립트의 함수는 양면의 얼굴 => 생성자(constructor)
+ // 생성자로만 사용하세요
+ // 관례 : 생성자 함수는 대문자 시작으로 
+// function Button (text){
+
+// 	this.content = text; // this => 나를 호출한 대상 
+
+// };
+// const b = new Button('more') // b = this 
+// 생성자 함수는 객체를 return 함
+
+
+// 일반함수 
+// - constructor 내장
+// - this : 나를 호출한 대상 this
+// - 객체의 매서드로 사용이 많이 됨 => this를 찾기 위해
+
+// 화살표 함수 
+// - constructor 비내장
+// - this : 바인딩 하지 않음 -> 상위 컨텍스트에서 this 찾음. 
+
+// 예시 1) 
+// const user = {
+// 	name: '정은선',
+// 	sayHi: function(){
+// 		console.log(this);  // this = 나를 호출한 user
+// 	},
+// 	sayHi2:()=>{
+// 		console.log(this);
+// 	},
+// 	sayHi3(){
+// 		console.log(this);
+// 	}
+// } 
+
+// 예시 2)
+// const user = {
+// 	name: '박새롬',
+// 	grades: [30, 60, 80],
+// 	totalGrades(){
+// 		this.grades.forEach(function(item){
+// 			this.total += item
+// 		})
+// 		return this.total
+// 	},
+// 	sayHi(){
+// 		console.log(`안녕 나는 ${this.name} 나는 아주 멋쟁이야~`);
+// 	}
+// }
+
+
+// 예시3
+// const user = {
+// 	name: '박새롬',
+// 	total: 0,
+// 	grades: [30, 60, 80],
+// 	totalGrades(){
+// 		console.log(this.totalGrades);
+		
+// 		function sayHi(){
+// 			console.log(this);
+// 		}
+
+// 		sayHi()
+// 	}
+// }
+
+
+// 예시3 변형
+const user = {
+	name: '박새롬',
+	total: 0,
+	grades: [30, 60, 80],
+	totalGrades(){
+		
+		const sayHi = () => {
+			console.log(this);
+		}
+
+		sayHi()
+	}
+}
+
+
+
+
+
+
   
   /* 다음 함수를 작성해봅니다. -------------------------------------------------- */
   
