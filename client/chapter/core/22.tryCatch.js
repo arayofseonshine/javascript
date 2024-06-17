05fee58201a3276671311166682df4f55d309438
arayofseonshine <arayofseonshine@naver.com>
2024-06-17 13:50:48 +0900

    learn.DOM
    -초기 환경 구성

--- client/chapter/core/22.tryCatch.js
+++ client/chapter/core/22.tryCatch.js
@@ -41,29 +41,10 @@ try{
   }
 
 
-  {
-    "id": 1,
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz",
-    "address": {
-      "street": "Kulas Light",
-      "suite": "Apt. 556",
-      "city": "Gwenborough",
-      "zipcode": "92998-3874",
-      "geo": {
-        "lat": "-37.3159",
-        "lng": "81.1496"
-      }
-    },
-    "phone": "1-770-736-8031 x56442",
-    "website": "hildegard.org",
-    "company": {
-      "name": "Romaguera-Crona",
-      "catchPhrase": "Multi-layered client-server neural-net",
-      "bs": "harness real-time e-markets"
-    }
-  }
-// finally 절
+
+
+
+
+// finally 절c
 // 오류가 있던, 없던 상관없이 항상 실행
 // try ... catch를 빠져나가는 어떠한 경우에도 항상 실행
